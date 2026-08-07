6542f1d052ca31d883b74a34671d39644fdee208
NIHKIL1010 <kharatnikhil1010@gmail.com>
2026-08-07 23:54:10 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      int l=0;
+      int ans=0;
+      unordered_set<char> st;
+      for(int r=0;r<s.size();r++){
+        //h awhile loop ahe karan apalyaya remove hoyacha ahe toparyant kadaycha ahe tip : dry run 
+        while(st.count(s[r])){
+            st.erase(s[l]);
+            l++;
+        }
+        st.insert(s[r]);
+        ans=max(ans,r-l+1);
+        
+      }
+      return ans;
+    }
+};
